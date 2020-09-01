9bfb04123cc2a83eb383a8cacda21d404227786c
Keenan Carr <keenan.carr@oregonstate.edu>
2020-09-01 16:04:21 -0700

    Added additional code checks

--- LogSoftware/run.ps1
+++ LogSoftware/run.ps1
@@ -3,10 +3,22 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
+Export-Clixml -InputObject $Request -Path .\notes\Request.cli.xml
+
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
-#Creating an object that contains all of the needed information
+# Making sure that all of the needed variables are available
+if ([string]::IsNullOrEmpty($env:AzureWebJobsStorage)) { Throw 'Could not find $env:AzureWebJobsStorage' }
+if ([string]::IsNullOrEmpty($env:TenantID)) { Throw 'Could not find $env:TenantID' }
+if ([string]::IsNullOrEmpty($env:ServicePrincipal)) { Throw 'Could not find $env:ServicePrincipal' }
+if ([string]::IsNullOrEmpty($env:ServicePrincipalSecret)) { Throw 'Could not find $env:ServicePrincipalSecret' }
+if ([string]::IsNullOrEmpty($env:StorageAccountName)) { Throw 'Could not find $env:StorageAccountName' }
+if ([string]::IsNullOrEmpty($env:StorageAccountKey)) { Throw 'Could not find $env:StorageAccountKey' }
+if ([string]::IsNullOrEmpty($env:AzureTableName)) { Throw 'Could not find $env:AzureTableName' }
+if ([string]::IsNullOrEmpty($env:PartitionKey)) { Throw 'Could not find $env:PartitionKey' }
+
+# Creating an object that contains all of the needed information
 $ProcessingObject = [hashtable]@{   
     # Gathering the Tenant ID
     Tenant                     = $env:TenantID
@@ -17,6 +29,23 @@ $ProcessingObject = [hashtable]@{
     StorageAccountKey          = $env:StorageAccountKey
     StorageTableName           = $env:AzureTableName
     PartitionKey               = $env:PartitionKey
+    # AzureWebJobsStorage        = [hashtable]$(
+    #     # Creating an empty hashtable
+    #     $temp = [hashtable]@{}
+    #     $(
+    #         # Looping through all of the parts in the variable
+    #         foreach ($item in $($env:AzureWebJobsStorage.split(";"))) {
+    #             # Setting the name and value
+    #             $tempname = $(($item -split "=", 2)[0])
+    #             $tempvalue = $(($item -split "=", 2)[1])
+    
+    #             # Adding them to the hashtable
+    #             $temp.Add($tempname, $tempvalue)
+    #         }
+    #     )
+    #     #Outputing the results
+    #     $temp
+    # )
     # Gathering the body of the request and converting it from JSON to a PSCustomObject
     StorageTableProperties     = $(
         $obj = $Request.Body | ConvertFrom-Json
@@ -24,6 +53,11 @@ $ProcessingObject = [hashtable]@{
         $obj.psobject.properties | ForEach-Object { $Hash[$_.Name] = $_.Value }
         $Hash
     )
+}
+
+# If the processing object does NOT contain the software name, break.
+if (! $ProcessingObject.StorageTableProperties.Contains("softwarename") ) {
+    break
 }
 
 # Load the functions
@@ -46,13 +80,13 @@ switch ($ProcessingObject) {
     { $_.Results.httpstatuscode -eq 204 } {
         $status = @{
             HttpStatusCode = [string]"OK"
-            Body = [string]"Successfully Logged Install"
+            Body           = [string]"Successfully Logged Install"
         }
     }
     { [string]::IsNullOrEmpty($_.Results) } {
         $status = @{
             HttpStatusCode = [string]"BadRequest"
-            Body = [string]"Failed to process request"
+            Body           = [string]"Failed to process request"
         }
     }
     Default {
@@ -65,6 +99,4 @@ switch ($ProcessingObject) {
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 GenerateReponseObject @Status
 
-$ProcessingObject += @{Status = $status}
-
-Export-Clixml -InputObject $ProcessingObject -Force .\ProcessingObject.cli.xml
+Export-Clixml -InputObject $ProcessingObject -Path .\notes\ProcessingObject.cli.xml
