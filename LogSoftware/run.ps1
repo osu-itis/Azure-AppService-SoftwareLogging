e0e35253a2353d2978e1076079806143a7b0c817
Keenan Carr <keenan.carr@oregonstate.edu>
2020-09-01 16:53:07 -0700

    Using built in Output Binding Using built in AzureWebJobsStorage variable for connection

--- LogSoftware/run.ps1
+++ LogSoftware/run.ps1
@@ -3,54 +3,23 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
-Export-Clixml -InputObject $Request -Path .\notes\Request.cli.xml
-
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
 # Making sure that all of the needed variables are available
 if ([string]::IsNullOrEmpty($env:AzureWebJobsStorage)) { Throw 'Could not find $env:AzureWebJobsStorage' }
-if ([string]::IsNullOrEmpty($env:TenantID)) { Throw 'Could not find $env:TenantID' }
-if ([string]::IsNullOrEmpty($env:ServicePrincipal)) { Throw 'Could not find $env:ServicePrincipal' }
-if ([string]::IsNullOrEmpty($env:ServicePrincipalSecret)) { Throw 'Could not find $env:ServicePrincipalSecret' }
-if ([string]::IsNullOrEmpty($env:StorageAccountName)) { Throw 'Could not find $env:StorageAccountName' }
-if ([string]::IsNullOrEmpty($env:StorageAccountKey)) { Throw 'Could not find $env:StorageAccountKey' }
-if ([string]::IsNullOrEmpty($env:AzureTableName)) { Throw 'Could not find $env:AzureTableName' }
 if ([string]::IsNullOrEmpty($env:PartitionKey)) { Throw 'Could not find $env:PartitionKey' }
 
 # Creating an object that contains all of the needed information
-$ProcessingObject = [hashtable]@{   
-    # Gathering the Tenant ID
-    Tenant                     = $env:TenantID
-    # Gathering the credentials for the Service Principal account
-    ServicePrincipalCredential = New-Object System.Management.Automation.PSCredential ("$ENV:ServicePrincipal", $(ConvertTo-SecureString "$($ENV:ServicePrincipalSecret)" -AsPlainText -Force))
-    # Gathering all of the needed storage settings
-    StorageAccountName         = $env:StorageAccountName
-    StorageAccountKey          = $env:StorageAccountKey
-    StorageTableName           = $env:AzureTableName
-    PartitionKey               = $env:PartitionKey
-    # AzureWebJobsStorage        = [hashtable]$(
-    #     # Creating an empty hashtable
-    #     $temp = [hashtable]@{}
-    #     $(
-    #         # Looping through all of the parts in the variable
-    #         foreach ($item in $($env:AzureWebJobsStorage.split(";"))) {
-    #             # Setting the name and value
-    #             $tempname = $(($item -split "=", 2)[0])
-    #             $tempvalue = $(($item -split "=", 2)[1])
-    
-    #             # Adding them to the hashtable
-    #             $temp.Add($tempname, $tempvalue)
-    #         }
-    #     )
-    #     #Outputing the results
-    #     $temp
-    # )
+$ProcessingObject = [hashtable]@{
     # Gathering the body of the request and converting it from JSON to a PSCustomObject
     StorageTableProperties     = $(
         $obj = $Request.Body | ConvertFrom-Json
         $Hash = [ordered]@{}
         $obj.psobject.properties | ForEach-Object { $Hash[$_.Name] = $_.Value }
+        # Adding the needed partition key and row key
+        $Hash.Add("PartitionKey",$env:PartitionKey)
+        $Hash.Add("RowKey", $(new-guid | Select-Object -ExpandProperty guid))
         $Hash
     )
 }
@@ -61,42 +30,20 @@ if (! $ProcessingObject.StorageTableProperties.Contains("softwarename") ) {
 }
 
 # Load the functions
-. ".\LogSoftware\New-PostToTable.ps1"
 . ".\LogSoftware\GenerateReponseObject.ps1"
 
 # Attempt to post to the table and collect the results
 try {
-    $temp = @{Results = $( New-PostToTable @ProcessingObject ) }
+    # Pushing to the table
+    Push-OutputBinding -Name "outputTable" -Value $ProcessingObject.StorageTableProperties
+    # Setting the status
+    $status = @{
+        HttpStatusCode = [string]"OK"
+        Body           = [string]"Successfully Logged Install"
+    }
+    # Associate values to output bindings by calling 'Push-OutputBinding'.
+    GenerateReponseObject @Status
 }
 catch {
-    $temp = @{Results = $null }    
+    Break
 }
-
-# Add the results to the Main Object
-$ProcessingObject += $temp
-
-# Determine the final status of the function
-switch ($ProcessingObject) {
-    { $_.Results.httpstatuscode -eq 204 } {
-        $status = @{
-            HttpStatusCode = [string]"OK"
-            Body           = [string]"Successfully Logged Install"
-        }
-    }
-    { [string]::IsNullOrEmpty($_.Results) } {
-        $status = @{
-            HttpStatusCode = [string]"BadRequest"
-            Body           = [string]"Failed to process request"
-        }
-    }
-    Default {
-        $status = @{
-            HttpStatusCode = [string]"InternalServerError"
-        }
-    }
-}
-
-# Associate values to output bindings by calling 'Push-OutputBinding'.
-GenerateReponseObject @Status
-
-Export-Clixml -InputObject $ProcessingObject -Path .\notes\ProcessingObject.cli.xml
--- README.md
+++ README.md
@@ -20,14 +20,7 @@ Software logging saved to Azure table storage
 - Create App Function
   - Add application insights (software monitoring)
   - Configure:
-    - Application Settings
-      - Add storage account name and key (so they are env variables to the script)
-      - Add service principal and secret (AKA the app registration's client ID and secret)  (if needed to connect and azure and load the `aztablerow` commands)
-  - New Function
-    - Remove "get" from the function.json
-    - Set function.json to "anonymous" rather than function
-    - Copy code to run.ps1
-    - Copy code to requirements.psd1
+    - git clone code or copy code to the App Function
   - Custom Domains
     - Enable HTTPS Only
 
